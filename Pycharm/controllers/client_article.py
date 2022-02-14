a5f55643c68ee49905274de16f9a6e72bdbf071e
VILLEROT Justin <justinvillerot@orange.fr>
2022-02-14 21:23:08 +0100

    Filter Client
    Taille buger reste a corriger

--- Pycharm/controllers/client_article.py
+++ Pycharm/controllers/client_article.py
@@ -13,11 +13,95 @@ client_article = Blueprint('client_article', __name__,
 @client_article.route('/client/article/show')      # remplace /client
 def client_article_show():                                 # remplace client_index
     mycursor = get_db().cursor()
-    sql='''SELECT Produit.idProduit, LibelleProduit, Prix, Description, Stock, imageProduit
-        FROM Produit
-    INNER JOIN Variations V2 on Produit.idProduit = V2.idProduit;'''
-    mycursor.execute(sql)
+    sql = '''SELECT Produit.*, V2.*
+        FROM Produit 
+        INNER JOIN Variations V2 on Produit.idProduit = V2.idProduit'''
+    list_param = []
+    condition_and = ""
+    if "filter_word" in session \
+            or "filter_Grade" in session \
+            or "filter_Taille" in session \
+            or "filter_Fournisseur" in session \
+            or "filter_prix_min" in session \
+            or "filter_prix_max" in session \
+            or "filter_type_article" in session:
+        sql = sql + " WHERE "
+
+    print()
+    print()
+    print("Show article")
+    if "filter_word" in session:
+        print(1)
+        sql = sql + " Produit.LibelleProduit LIKE %s"
+        print(2)
+        print(sql)
+        recherche = "%" + session["filter_word"] + "%"
+        print('3 ' + recherche)
+        list_param.append(recherche)
+        print('4 ')
+        print(list_param)
+        condition_and = " AND "
+        print(5)
+
+    print('test prix ')
+    if "filter_prix_min" in session or "filter_prix_max" in session:
+        sql = sql + condition_and + " Produit.Prix BETWEEN %s AND %s "
+        list_param.append(session["filter_prix_min"])
+        list_param.append(session["filter_prix_max"])
+        condition_and = " AND "
+
+    print('test Type article ')
+    if "filter_type_article" in session:
+        sql = sql + condition_and + "("
+        last_type = session["filter_type_article"][-1]
+        for item in session['filter_type_article']:
+            sql = sql + " Produit.idType = %s "
+            if item != last_type:
+                sql = sql + " or "
+            list_param.append(item)
+        sql = sql + ")"
+
+    print('test Grade article ')
+    if "filter_Grade" in session:
+        sql = sql + condition_and + "("
+        last_grade = session["filter_Grade"][-1]
+        for item in session['filter_Grade']:
+            sql = sql + " Produit.idGrade = %s "
+            if item != last_grade:
+                sql = sql + " or "
+            list_param.append(item)
+        sql = sql + ")"
+
+    print('test Taille article ')
+    if "filter_Taille" in session:
+        sql = sql + condition_and + "("
+        last_Taille = session["filter_Taille"][-1]
+        for item in session['filter_Taille']:
+            sql = sql + " Produit.idTaille = %s "
+            if item != last_Taille:
+                sql = sql + " or "
+            list_param.append(item)
+        sql = sql + ")"
+
+    print('test Taille article ')
+    if "filter_Fournisseur" in session:
+        sql = sql + condition_and + "("
+        last_Fournisseur = session["filter_Fournisseur"][-1]
+        for item in session['filter_Fournisseur']:
+            sql = sql + " Produit.idFourniseur = %s "
+            if item != last_Fournisseur:
+                sql = sql + " or "
+            list_param.append(item)
+        sql = sql + ")"
+
+    print('resultat fin')
+    tuple_sql = tuple(list_param)
+    print(sql)
+    print(list_param)
+    mycursor.execute(sql, tuple_sql)
     article = mycursor.fetchall()
+
+
     articles_panier = []
     sql="SELECT * FROM PanierUser"
     mycursor.execute(sql)
@@ -30,11 +114,26 @@ def client_article_show():                                 # remplace client_ind
     mycursor.execute(sql)
     filter_type_article = mycursor.fetchall()
 
+    sql = "SELECT * FROM Grade"
+    mycursor.execute(sql)
+    filter_Grade = mycursor.fetchall()
+
+    sql = "SELECT * FROM Taille"
+    mycursor.execute(sql)
+    filter_Taille = mycursor.fetchall()
+
+    sql = "SELECT * FROM Fourniseur"
+    mycursor.execute(sql)
+    filter_Fournisseur = mycursor.fetchall()
+
     return render_template('client/boutique/panier_article.html'
-                           ,  article=article
+                           , article=article
                            , articlesPanier=articles_panier
                            , prix_total=prix_total
-                           , filter_type_article=filter_type_article)
+                           , filter_type_article=filter_type_article
+                           , filter_Grade=filter_Grade
+                           , filter_Taille=filter_Taille
+                           , filter_Fournisseur=filter_Fournisseur)
 
 @client_article.route('/client/article/details/<int:id>', methods=['GET'])
 def client_article_details(id):
--- Pycharm/controllers/client_panier.py
+++ Pycharm/controllers/client_panier.py
@@ -6,7 +6,7 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 from connexion_db import get_db
 
 client_panier = Blueprint('client_panier', __name__,
-                        template_folder='templates')
+                          template_folder='templates')
 
 
 @client_panier.route('/client/panier/add', methods=['POST'])
@@ -14,14 +14,15 @@ def client_panier_add():
     mycursor = get_db().cursor()
 
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
+
 
 @client_panier.route('/client/panier/delete', methods=['POST'])
 def client_panier_delete():
     mycursor = get_db().cursor()
 
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
 
 
 @client_panier.route('/client/panier/vider', methods=['POST'])
@@ -29,7 +30,7 @@ def client_panier_vider():
     mycursor = get_db().cursor()
 
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
 
 
 @client_panier.route('/client/panier/delete/line', methods=['POST'])
@@ -37,32 +38,100 @@ def client_panier_delete_line():
     mycursor = get_db().cursor()
 
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
 
 
 @client_panier.route('/client/panier/filtre', methods=['POST'])
 def client_panier_filtre():
     mycursor = get_db().cursor()
-
-
-    # SQL
-
-
     filter_word = request.form.get('filter_word', None)
+    filter_type_article = request.form.getlist('filter_type_article', None)
+    filter_Grade = request.form.getlist('filter_Grade', None)
+    filter_Taille = request.form.getlist('filter_Taille', None)
+    filter_Fournisseur = request.form.getlist('filter_Fournisseur', None)
     filter_prix_min = request.form.get('filter_prix_min', None)
     filter_prix_max = request.form.get('filter_prix_max', None)
-    filter_types = request.form.getlist('filter_types', None)
+    print("word: " + filter_word + str(len(filter_word)))
+
+    if filter_word or filter_word == "":
+        if len(filter_word) > 1:
+            if filter_word.isalpha():
+                session['filter_word'] = filter_word
+            else:
+                flash(u'votre Mot recherché doit uniquement être composé de lettres')
+        else:
+            if len(filter_word) == 1:
+                flash(u'votre Mot recherché doit être composé d\'au moins 2 lettres')
+            else:
+                session.pop('filter_word', None)
+
+    if filter_type_article and filter_type_article != []:
+        print("filter_type_article : ", filter_type_article)
+        if isinstance(filter_type_article, list):
+            check_filter_type_article = True
+            for number_type_article in filter_type_article:
+                print('test', number_type_article)
+                if not number_type_article.isdecimal():
+                    check_filter_type_article = False
+                if check_filter_type_article:
+                    session['filter_type_article'] = filter_type_article
+                    print(session['filter_type_article'])
+
+    if filter_Grade and filter_Grade != []:
+        print("filter_Grade : ", filter_Grade)
+        if isinstance(filter_Grade, list):
+            check_filter_Grade = True
+            for number_departement in filter_Grade:
+                print('test', number_departement)
+                if not number_departement.isdecimal():
+                    check_filter_Grade = False
+                if check_filter_Grade:
+                    session['filter_Grade'] = filter_Grade
+
+    if filter_Taille and filter_Taille != []:
+        print("filter_Taille : ", filter_Taille)
+        if isinstance(filter_Taille, list):
+            check_filter_Taille = True
+            for number_departement in filter_Taille:
+                print('test', number_departement)
+                if not number_departement.isdecimal():
+                    check_filter_Taille = False
+                if check_filter_Taille:
+                    session['filter_Taille'] = filter_Taille
+
+    if filter_Fournisseur and filter_Fournisseur != []:
+        print("filter_Fournisseur : ", filter_Fournisseur)
+        if isinstance(filter_Fournisseur, list):
+            check_filter_Fournisseur = True
+            for number_departement in filter_Fournisseur:
+                print('test', number_departement)
+                if not number_departement.isdecimal():
+                    check_filter_Fournisseur = False
+                if check_filter_Fournisseur:
+                    session['filter_Fournisseur'] = filter_Fournisseur
+
+    print("filter_prix : ")
+    if filter_prix_min or filter_prix_max:
+        if filter_prix_min < filter_prix_max:
+            session['filter_prix_min'] = filter_prix_min
+            session['filter_prix_max'] = filter_prix_max
+        else:
+            flash(u'min < max')
+
 
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
 
 
 @client_panier.route('/client/panier/filtre/suppr', methods=['POST'])
 def client_panier_filtre_suppr():
     session.pop('filter_word', None)
+    session.pop('filter_type_article', None)
+    session.pop('filter_Grade', None)
+    session.pop('filter_Taille', None)
+    session.pop('filter_Fournisseur', None)
     session.pop('filter_prix_min', None)
     session.pop('filter_prix_max', None)
-    session.pop('filter_types', None)
     print("suppr filtre")
     return redirect('/client/article/show')
-    #return redirect(url_for('client_index'))
+    # return redirect(url_for('client_index'))
